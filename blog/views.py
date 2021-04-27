4ea4fb76ebae9a584b7adc1f7e703818de4f5581
luqbryann <77641018+luqbryann@users.noreply.github.com>
2021-04-27 17:34:31 -0500

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,8 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+ 
+def index(request):
+  return  render (request,'index.html') 
+    
+
